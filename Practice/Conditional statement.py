3d3ea6aaedcb96a91f8aaf2ddd9787ea44a0ad98
𝙎𝙪𝙣𝙞𝙡 𝙎𝙝𝙖𝙧𝙢𝙖 <ji2796709@gmail.com>
2025-12-03 21:30:52 +0530

    Add files via upload

---
+++ Practice/Conditional statement.py
@@ -0,0 +1,44 @@
+#1 program
+
+# age = int(input("Enter your age: "))
+# if (age < 13):
+#     print("child")
+# elif (age > 13 and age < 18):
+#     print("teenager")
+# else:
+#     print("adult")
+
+
+
+#2 program
+
+# username = input("Enter your username: ")
+# password = input("Enter your password: ")
+# if (username == "Sunil" and password == "Sharma"):
+#     print("Login Successful")
+# elif (username != "Sunil"):
+#     print("wrong username")
+# else:
+#     print("wrong password")
+
+
+#3 program
+
+# n = int(input("enter a number: "))
+#
+# if (n % 5 == 0):
+#     print("multiple of 5")
+# else:
+#     print("not multiple of 5")
+
+#4 program
+
+# n = int(input("Enter a number: "))
+#
+# if (n % 2) == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+#5 program
+
